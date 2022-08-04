27bd419d5eef88643810cf1ba95405e1216e4fdc
gary <gary.white@lodgit.com>
2022-08-04 12:56:40 +0200

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function DNAStrand(dna) {
+    //your code here
+}
+
+console.log(DNAStrand("AAAA")); // "TTTT"
+console.log(DNAStrand("ATTGC")); // "TAACG"
+console.log(DNAStrand("GTAT")); // "CATA"
