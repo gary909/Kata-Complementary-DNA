86a2b784d848404e299083a52cd20b932cbfc897
gary <gary.white@lodgit.com>
2022-08-04 13:04:14 +0200

    code working

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 function DNAStrand(dna) {
-    //your code here
+    let myStr = "";
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] == "A") {
+            myStr = myStr + "T";
+        } else if (dna[i] == "T") {
+            myStr = myStr + "A";
+        } else if (dna[i] == "C") {
+            myStr = myStr + "G";
+        } else if (dna[i] == "G") {
+            myStr = myStr + "C";
+        }
+    }
+    return myStr;
 }
+
 
 console.log(DNAStrand("AAAA")); // "TTTT"
 console.log(DNAStrand("ATTGC")); // "TAACG"
